cc3df0ce8d7f93938e6ef04c7068dd26b13a62bb
heiy <287789299@qq.com>
2019-12-11 16:38:38 +0800

    优化 70.爬楼梯问题代码

--- 70.爬楼梯.go
+++ 70.爬楼梯.go
@@ -18,16 +18,11 @@ func climbStairs(n int) int {
 	if n == 1 {
 		return 1
 	}
-	if n == 2 {
-		return 2
-	}
-	f1, f2, f3 := 1, 2, 3
+	f1, f2 := 1, 2
 	for i := 2; i < n; i++ {
-		f3 = f1 + f2
-		f1 = f2
-		f2 = f3
+		f1, f2 = f2, f1+f2
 	}
-	return f3
+	return f2
 }
 
 // @lc code=end
